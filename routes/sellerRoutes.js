ac7f9e8f161e9147eb9ab3513dcde37337b3782b
JESWINTJ <146103442+JESWINTJ@users.noreply.github.com>
2025-06-30 18:50:55 +0530

    Update sellerRoutes.js

--- routes/sellerRoutes.js
+++ routes/sellerRoutes.js
@@ -6,8 +6,10 @@ import {
   getSellerProducts,
   addNewProduct,
   updateProduct,
-  getSellerOrders
+  getSellerOrders,updateSellerProfile
 } from '../controllers/sellerController.js';
+
+
 
 import { authenticateUser as protect } from '../middleware/authentication.js';
 
@@ -18,10 +20,16 @@ router.post('/register', registerSeller);
 
 // Protected (Seller only routes)
 router.get('/check', protect, checkSeller);
-router.get('/profile', protect, viewSellerProfile);
+
 router.get('/products', protect, getSellerProducts);
 router.post('/products', protect, addNewProduct);
 router.put('/products/:id', protect, updateProduct);
 router.get('/orders', protect, getSellerOrders);
+
+router
+  .route('/profile')
+  .get(protect, viewSellerProfile)
+  .put(protect, updateSellerProfile); // instead of /updateprofile
+
 
 export default router;
