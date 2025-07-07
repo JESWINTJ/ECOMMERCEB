60327a635d4ed6a7287a3cd92804ec33e27a2fb2
JESWINTJ <146103442+JESWINTJ@users.noreply.github.com>
2025-07-08 02:27:08 +0530

    Update app.js

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use('/api', router);
 
 // Global Error Handler
 app.use((err, req, res, next) => {
-  console.error(err.stack);
+  console.error(err.stack); 
   res.status(err.statusCode || 500).json({
     message: err.message || 'Internal Server Error',
   });
